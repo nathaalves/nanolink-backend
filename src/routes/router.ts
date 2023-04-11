7dd4284055fe1651d6ada56e7f4cae8f3ccde7a8
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-10 21:09:19 -0300

    feat: ccreate router

---
+++ src/routes/router.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { shortLinkController } from '../controllers/shortLinkController';
+import { linkRouter } from './shortLinkRouter';
+
+const router = Router();
+
+router.use('/short-link', linkRouter);
+router.use('/', shortLinkController.goToHomeApp);
+
+export { router };
