01b25c8732a811603ed53dd381fae27b0b4b4d7c
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-10 21:08:48 -0300

    feat: create link router

---
+++ src/routes/shortLinkRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { shortLinkController } from '../controllers/shortLinkController';
+import { validateSchema } from '../middlewares/validateSchema';
+import { linkBodySchema, linkParamsSchema } from '../schemas/shortLinkSchema';
+
+const linkRouter = Router();
+
+linkRouter.post(
+  '/create',
+  validateSchema.body(linkBodySchema),
+  shortLinkController.create
+);
+
+linkRouter.get(
+  '/:id',
+  validateSchema.params(linkParamsSchema),
+  shortLinkController.getURL
+);
+
+export { linkRouter };
