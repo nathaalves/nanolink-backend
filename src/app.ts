b6de2bdc201150d3ba4ac71baa7e56f8caf0eb10
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-10 21:10:03 -0300

    feat: crceate app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
+import 'express-async-errors';
 import cors from 'cors';
 import express from 'express';
 import { router } from './routes/router';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
+
+app.use(errorHandler);
 
 export { app };
