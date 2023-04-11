bee34d3dffcfb761b873e64925ba46d196a49bd0
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-10 21:08:16 -0300

    feat: create link controller

---
+++ src/controllers/shortLinkController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { shortLinkService } from '../services/shortLinkService';
+import { ShortLinkRequestBodyType } from '../types/shortLinkTypes';
+
+async function create(req: Request, res: Response) {
+  const data: ShortLinkRequestBodyType = req.body;
+
+  const url = await shortLinkService.addId(data);
+
+  res.status(201).send(url);
+}
+
+async function getURL(req: Request, res: Response) {
+  const id: string = req.params.id;
+
+  const { url } = await shortLinkService.getURL(id);
+
+  res
+    .writeHead(301, {
+      Location: url,
+    })
+    .end();
+}
+
+async function goToHomeApp(_req: Request, res: Response) {
+  res
+    .writeHead(301, {
+      Location: `https://nanolink.app.br`,
+    })
+    .end();
+}
+
+export const shortLinkController = {
+  create,
+  getURL,
+  goToHomeApp,
+};
