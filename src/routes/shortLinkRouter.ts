fea671ec175494ebc8dbb78030c02801bea5ef98
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-17 15:29:03 -0300

    feat: add shortlink middleware

--- src/routes/shortLinkRouter.ts
+++ src/routes/shortLinkRouter.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { shortLinkController } from '../controllers/shortLinkController';
 import { validateSchema } from '../middlewares/validateSchema';
 import { linkBodySchema, linkParamsSchema } from '../schemas/shortLinkSchema';
+import { shortLinkMiddleware } from '../middlewares/shortLinkMiddleware';
 
 const linkRouter = Router();
 
 linkRouter.post(
   '/create',
   validateSchema.body(linkBodySchema),
+  shortLinkMiddleware.verifyIfURLExists,
   shortLinkController.create
 );
 
