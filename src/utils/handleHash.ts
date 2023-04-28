24c04ba158e98576418f688cf32d4dfc0eb33f11
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-28 18:01:21 -0300

    feat: create handle hash functions

---
+++ src/utils/handleHash.ts
@@ -0,0 +1,17 @@
+import { compareSync, genSaltSync, hashSync } from 'bcrypt';
+
+function createHash(data: string) {
+  const SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS) || 12;
+
+  const salt = genSaltSync(SALT_ROUNDS);
+
+  const encryptedData = hashSync(data, salt);
+
+  return encryptedData;
+}
+
+function compareHash(data: string, hash: string) {
+  return compareSync(data, hash);
+}
+
+export { createHash, compareHash };
