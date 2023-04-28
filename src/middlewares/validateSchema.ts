70cda5663de3a647c2aa32c571882ed57c206dc1
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-28 18:11:55 -0300

    fix: fix response locals key type

--- src/middlewares/validateSchema.ts
+++ src/middlewares/validateSchema.ts
@@ -1,14 +1,12 @@
 import { ZodObject, ZodRawShape } from 'zod';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, Locals } from 'express';
 
 function validate<T extends ZodRawShape>(
   schema: ZodObject<T>,
   type: 'body' | 'params'
 ) {
   return (req: Request, res: Response, next: NextFunction) => {
-    res.locals[type as keyof Response] = schema.parse(
-      req[type as keyof Request]
-    );
+    res.locals[type as keyof Locals] = schema.parse(req[type as keyof Request]);
     next();
   };
 }
