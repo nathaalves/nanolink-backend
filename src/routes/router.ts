624bee7e717132bebe71354ab0ad943c017f396e
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-28 21:43:28 -0300

    feat: create route /auth/signup

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { linkRouter } from './shortLinkRouter';
+import { authRouter } from './authRouter';
 
 const router = Router();
 
 router.use(linkRouter);
+router.use('/auth', authRouter);
 
 export { router };
