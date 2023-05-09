6d6fa99cbc229237b021597a509d074537412741
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-09 20:34:12 -0300

    feat: chage error messages

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -5,7 +5,7 @@ export const registrationSchema = z.object({
     .string({
       required_error: 'O campo nome é obrigatório',
     })
-    .min(1),
+    .min(1, { message: 'O nome deve ter no mínimo 1 caractere.' }),
   email: z
     .string({
       required_error: 'O campo e-mail é obrigatório',
@@ -21,7 +21,9 @@ export const registrationSchema = z.object({
     .string({
       required_error: 'O campo confirmação de senha é obrigatório',
     })
-    .min(8, { message: 'A senha deve ter no mínimo 8 caracteres.' }),
+    .min(8, {
+      message: 'A confirmação da senha deve ter no mínimo 8 caracteres.',
+    }),
 });
 
 export const loginSchema = z.object({
